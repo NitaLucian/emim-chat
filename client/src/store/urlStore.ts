a883c1d17f1353470c0e24ee5717beb97486656f
NitaLucian <lucien_nita@yahoo.com>
2022-02-16 12:30:01 +0200

    url emim

--- client/src/store/urlStore.ts
+++ client/src/store/urlStore.ts
@@ -2,8 +2,9 @@
 
 export default class UrlStore{
 
-    //private static urlEmim: string = 'http://localhost:5006/';
-    private static urlEmim: string = 'http://82.77.27.57:56002/';
+    //private static urlEmim: string = 'http://localhost:5006/'; https://telemedicina.emim.ro:444/api/medici
+    //private static urlEmim: string = 'http://82.77.27.57:56002/';
+    private static urlEmim: string = 'https://telemedicina.emim.ro:444/';
     static urlEmimChat: string = this.urlEmim + 'chat';
     static urlEmimApi: string = this.urlEmim + 'api';
     static urlVideoChat: string = 'https://video-chat-luci.herokuapp.com/';
